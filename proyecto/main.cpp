6f675ab31e8de25994f9560092a804c6f04804cf
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-11 01:12:14 -0500

    Declaracion de las variables principales en main

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -9,7 +9,17 @@
 
 using namespace std;
 
+const int fil = 11; // Cantidad de mesas
+const int col = 6; // Cantidad de sillas por mesa
+
 int main (){
+	string mat[11][6]; // Representa la disposición de la biblioteca
+	Prestamo reg[100]; // Registro acumulado de préstamos del dia
+	int cant = 0; // Contador de préstamos activos
+	int opc; // La opción elegida por el usuario
+	
+	
+	
 	
 	
 	
