13f86e30e788539c23b31928efb06ec97f9762a3
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-11 10:16:34 -0500

    Se declara las variables de la funcion y se pide los datos al usuario

--- proyecto/ingreso_prestamo.cpp
+++ proyecto/ingreso_prestamo.cpp
@@ -7,7 +7,21 @@
 using namespace std;
 
 void registrarIngreso(string mat[11][6]) {
+	string id_estudiante;
+	int mesa, silla; // Recibido por el usuario
+	int f, c; // Indices reales de la matriz
 	
+	cout << "-------- REGISTRAR INGRESO DE ESTUDIANTE --------" << endl;
+	cout << "Ingrese el codigo del estudiante: ";
+	cin >> id_estudiante;
 	
+	cout << "Ingrese el numero de mesa (1 a 11): ";
+	cin >> mesa;
+	
+	cout << "Ingrese el numero de silla (1 a 6): ";
+	cin >> silla;
+	
+	f = mesa - 1; // Restamos para que encaje con los indices de C++
+	c = silla - 1;
 	
 }
