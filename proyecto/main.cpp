e58ab34622c492a203d6e4ac637e6794d8daa0c0
Valeria Maquera <vmaqueraj@unjbg.edu.pe>
2026-07-11 01:45:41 -0500

    Se completa el esqueleto general del menu

--- proyecto/main.cpp
+++ proyecto/main.cpp
@@ -50,28 +50,36 @@ int main (){
         // Switch de opciones
         switch(opc) {
         	case 1:
-        		cout << "Opcion en desarrollo por Valeria" << endl
-        		break;
+        		cout << "Opcion en desarrollo por Valeria" << endl;
+        		system("pause");
+				break;
         	case 2:
-        		cout << "Opcion en desarrollo por Valeria" << endl
-        		break;
+        		cout << "Opcion en desarrollo por Valeria" << endl;
+        		system("pause");
+				break;
         	case 3:
-        		cout << "Opcion en desarrollo por Zaida" << endl
-        		break;
+        		cout << "Opcion en desarrollo por Zaida" << endl;
+        		system("pause");
+				break;
         	case 4:
-        		cout << "Opcion en desarrollo por Zaida" << endl
-        		break;
+        		cout << "Opcion en desarrollo por Zaida" << endl;
+        		system("pause");
+				break;
         	case 5:
-        		cout << "Opcion en desarrollo por Lia" << endl
-        		break;
+        		cout << "Opcion en desarrollo por Lia" << endl;
+        		system("pause");
+				break;
         	case 6:
-        		cout << "Opcion en desarrollo por Lia" << endl
-        		break;
+        		cout << "Opcion en desarrollo por Lia" << endl;
+        		system("pause");
+				break;
         	case 7:
         		cout << "Saliendo del sistema UNIBIBLIO, hasta pronto." << endl;
-        		break;
+        		system("pause");
+				break;
         	default:
         		cout << "Opcion no valida. Intente de nuevo." << endl;
+        		system("pause");
 		}
         
         
