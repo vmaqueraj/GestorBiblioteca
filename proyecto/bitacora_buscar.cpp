12edb2fc9b1266093201c37dec10ded16b304d45
liatorresq <ltorresq@unjbg.edu.pe>
2026-07-12 16:03:04 -0500

    Se implementa bucle para listar recursos con espaciado

--- proyecto/bitacora_buscar.cpp
+++ proyecto/bitacora_buscar.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <string>
-#include <iomanip>
 #include "ingreso_prestamo.h"
 #include "bitacora_buscar.h"
 
 using namespace std;
 
+//opcion 5: bitácora de Recursos Prestados
 void mostrarBitacora(Prestamo reg[], int cant) {
-    cout << "\n==================================================\n";
-    cout << "     BITACORA DE RECURSOS PRESTADOS (ACTIVOS)     \n";
-    cout << "==================================================\n";
+    cout << "\n====================================\n";
+    cout << "     BITACORA DE RECURSOS PRESTADOS    \n";
+    cout << "=======================================\n";
     
     if (cant == 0) {
         cout << "No hay recursos prestados en este momento.\n";
         return;
     }
+    
+    cout << "Cod. Alumno     Tipo         ID Recurso      Titulo de Libro\n";
+    cout << "--------------------------------------------------------\n";
+
+    // Recorremos el arreglo
+    for (int i = 0; i < cant; i++) {
+        cout << reg[i].id_alu << "        " 
+             << reg[i].tipo << "         " 
+             << reg[i].id_rec << "         " 
+             << reg[i].tit << "\n";
+    }
+    cout << "==================================================\n";
 }
+
