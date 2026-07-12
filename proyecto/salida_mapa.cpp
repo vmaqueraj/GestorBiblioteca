0316cbd206c71903ac9d0c76688fefc1d629a3f3
zroquec <zroquec@unjbg.edu.pe>
2026-07-12 16:32:05 -0500

    Se optimiza validacion de existencia de estudiante y recurso en Opcion 3

--- proyecto/salida_mapa.cpp
+++ proyecto/salida_mapa.cpp
@@ -11,4 +11,18 @@ void procesarSalidaEstudiante(Prestamo reg[100], int &cant, string mat[11][6]) {
     string id_estudiante;
     cout << "Ingrese el codigo del estudiante que se retira: ";
     cin >> id_estudiante;
+    
+     //Verifica si el estudiante existe y que tipo de recurso tiene
+    bool existe_estudiante = false;
+    bool tiene_laptop = false;
+
+    for (int i = 0; i < cant; i++) {
+        if (reg[i].id_alu == id_estudiante) {
+            existe_estudiante = true;
+            if (reg[i].tipo == "Laptop") {
+                tiene_laptop = true;
+            }
+            break;
+        }
+    }
 }
