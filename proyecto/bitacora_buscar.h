ad57ae70bf362dbba75620638c7acb9108bb451c
liatorresq <ltorresq@unjbg.edu.pe>
2026-07-12 14:47:47 -0500

    Se crea la función mostrarBitacora para la opción 5 en el .h

---
+++ proyecto/bitacora_buscar.h
@@ -0,0 +1,12 @@
+#ifndef BITACORA_BUSCAR_H
+#define BITACORA_BUSCAR_H
+
+#include <string>
+#include "ingreso_prestamo.h"
+
+using namespace std;
+
+// Opcion 5
+void mostrarBitacora(Prestamo reg[100], int cant);
+
+#endif
