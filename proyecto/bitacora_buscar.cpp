71dd628c45a5a0b5d2926eb0857556ea68a315fa
liatorresq <ltorresq@unjbg.edu.pe>
2026-07-12 17:37:38 -0500

    Implementacion de funciones mostrarBitacora y buscarEstudiante en modulo bitacora_buscar.cpp

--- proyecto/bitacora_buscar.cpp
+++ proyecto/bitacora_buscar.cpp
@@ -29,11 +29,7 @@ void mostrarBitacora(Prestamo reg[], int cant) {
     cout << "==================================================\n";
 }
 
-void buscarEstudiantes(string mat[11][6], Prestamo reg[], int cant){
-	string bus;
-	cout << "\nIngrese el codigo del estudiante a buscar: ";
-	cin >> bus;
-}
+//opcion 6: buscar estudiante
 
 void buscarEstudiante(string mat[11][6], Prestamo reg[], int cant) {
     string bus; 
@@ -54,4 +50,38 @@ void buscarEstudiante(string mat[11][6], Prestamo reg[], int cant) {
         }
         if (m != -1) break;
     }
+    
+    int recursosEncontrados = 0;
+    for(int i = 0; i < cant; i++) {
+    	if (reg[i].id_alu == bus) {
+    		recursosEncontrados = recursosEncontrados + 1;
+		}
+	}
+	 cout << "\n==================================================\n";
+    cout << "              RESULTADO DE LA BUSQUEDA            \n";
+    cout << "==================================================\n";
+
+    if (m == -1 && recursosEncontrados == 0) {
+        cout << "Estudiante no registrado o activo en el sistema.\n";
+    } else {
+        cout << "Codigo Alumno: " << bus << "\n";
+        
+        if (m != -1) {
+            cout << "Ubicacion en Sala: Mesa " << m + 1 << ", Silla " << s + 1 << "\n";
+        } else {
+            cout << "Ubicacion en Sala: No se encuentra en la sala de estudio.\n";
+        }
+
+        cout << "Recursos Prestados:\n";
+        if (recursosEncontrados == 0) {
+            cout << "Ninguno.\n";
+        } else {
+            for (int i = 0; i < cant; i++) {
+                if (reg[i].id_alu == bus) {
+                    cout << "- " << reg[i].tipo << " (ID: " << reg[i].id_rec << ") - " << reg[i].tit << "\n";
+                }
+            }
+        }
+    }
+    cout << "==================================================\n";
 }
