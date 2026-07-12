7e82db1f10da085048fbce47bc545ed668cef7a5
liatorresq <ltorresq@unjbg.edu.pe>
2026-07-12 15:37:22 -0500

    Se agrega validacion de bitacora y el formato de cabecera

--- proyecto/bitacora_buscar.cpp
+++ proyecto/bitacora_buscar.cpp
@@ -5,3 +5,14 @@
 #include "bitacora_buscar.h"
 
 using namespace std;
+
+void mostrarBitacora(Prestamo reg[], int cant) {
+    cout << "\n==================================================\n";
+    cout << "     BITACORA DE RECURSOS PRESTADOS (ACTIVOS)     \n";
+    cout << "==================================================\n";
+    
+    if (cant == 0) {
+        cout << "No hay recursos prestados en este momento.\n";
+        return;
+    }
+}
