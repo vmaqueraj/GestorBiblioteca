4d769d2a98a15bd24ad2f1602a306d7ba73eebb3
zroquec <zroquec@unjbg.edu.pe>
2026-07-12 14:54:15 -0500

    Se agrega la lectura del codigo del estudiante para iniciar la busqueda

--- proyecto/salida_mapa.cpp
+++ proyecto/salida_mapa.cpp
@@ -5,3 +5,10 @@
 
 using namespace std;
 
+// Opcion 3
+void procesarSalidaEstudiante(Prestamo reg[100], int &cant, string mat[11][6]) {
+    cout << "\n-------- PROCESAR SALIDA Y DEVOLUCION --------" << endl;
+    string id_estudiante;
+    cout << "Ingrese el codigo del estudiante que se retira: ";
+    cin >> id_estudiante;
+}
