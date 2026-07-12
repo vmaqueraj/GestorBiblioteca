039b470677f67d006a66bf74e799ffc12b5933e9
zroquec <zroquec@unjbg.edu.pe>
2026-07-12 17:50:46 -0500

    Se integra visualizacion de la sala y calculo de totales

--- proyecto/salida_mapa.cpp
+++ proyecto/salida_mapa.cpp
@@ -78,7 +78,24 @@ void visualizarMapaSala(string mat[11][6]) {
     cout << "\n==============================================================================================" << endl;
     cout << "                                MAPA DE LA SALA DE ESTUDIO (11 MESAS)" << endl;
     cout << "==============================================================================================" << endl;
-    
     cout << "          Asiento 1       Asiento 2       Asiento 3       Asiento 4       Asiento 5       Asiento 6" << endl;
     cout << "----------------------------------------------------------------------------------------------" << endl;
+    
+    for (int i = 0; i < 11; i++) {
+        if (i < 9) cout << "MESA 0" << (i + 1) << ": ";
+        else cout << "MESA " << (i + 1) << ": ";
+
+        for (int j = 0; j < 6; j++) {
+            cout << mat[i][j];
+
+            if (mat[i][j] == "LIBRE") {
+                cout << "           ";
+                sillas_disponibles++;
+            } else {
+                cout << "      ";
+                estudiantes_en_sala++;
+            }
+        }
+        cout << endl; 
+    }
 }
